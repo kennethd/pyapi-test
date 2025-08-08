603fc5c4cd09d84a884cff17f169e74c9fd16308
Kenneth Dombrowski <kdombrowski@gmail.com>
2025-08-08 12:56:30 -0400

    inconsistent naming across modules

--- pyapi/flask/api.py
+++ pyapi/flask/api.py
@@ -8,9 +8,9 @@ log = getLogger(__name__)
 
 
 def configured_app(import_name, debug=False, config_module=None,
-                   profile=False, proxy_fix=False):
+                   profiler=False, proxy_fix=False):
     app = base_app(import_name, debug=debug, config_module=config_module,
-                   profile=profile, proxy_fix=proxy_fix)
+                   profiler=profiler, proxy_fix=proxy_fix)
     app.register_blueprint(FlaskAPIv1)
     log.debug("configured_app: {}".format(app))
     return app
